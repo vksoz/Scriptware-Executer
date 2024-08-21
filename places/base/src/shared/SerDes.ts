1ac871b7bf7f9dcee4178ab86bab203b1d487e79
NormalDuck <1juhuang2@duck.com>
2024-08-21 09:53:59 -0700

    style: format SerDes.ts

--- places/base/src/shared/SerDes.ts
+++ places/base/src/shared/SerDes.ts
@@ -1,6 +1,7 @@
 import { createBinarySerializer } from "@rbxts/flamework-binary-serializer";
-import { PlayerSave } from "./store/save/save-types";
+
 import { SharedState } from "./store";
+import { PlayerSave } from "./store/save/save-types";
 
 export const PlayerSerDes = createBinarySerializer<PlayerSave>();
 export const HydrateSerDes = createBinarySerializer<SharedState>();
